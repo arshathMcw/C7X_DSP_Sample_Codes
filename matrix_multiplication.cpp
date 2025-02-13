6f04191e297dc620938921b072aa883100af9eea
arshathahamed10 <arshathahamed392@gmail.com>
2025-02-13 11:31:37 +0530

    Works for 32 divided dimension

--- matrix_multiplication.cpp
+++ matrix_multiplication.cpp
@@ -5,10 +5,10 @@ cd scripts
 */
 #include "utils.h"
 int main(){
-    int row1 = 1;
-    int col1 = 32;
-    int row2 = 32;
-    int col2 = 32;
+    int row1 = 100;
+    int col1 = 96;
+    int row2 = 96;
+    int col2 = 320;
     int16_t mat1[row1][col1];
     int16_t mat2[row2][col2];
     int16_t res[row1][col2];
@@ -19,54 +19,54 @@ int main(){
     int iteration1, iteration2;
     for(int r = 0;r < row1;r++){
         for(int c =0;c < col1;c++){
-            mat1[r][c] =  r + c;
+            mat1[r][c] =  2;
         }
     }
     for(int r = 0;r < row2;r++){
         for(int c =0;c < col2;c++){
-            mat2[r][c] =  r + c;
-            // cout<<mat2[r][c]<<" ";
+            mat2[r][c] =  3;
         }
-        // cout<<endl;
     }
-    for(int r = 0;r < col1;r++){
-        for(int c =0;c < row2;c++){
-            for(int k = 0;k < row2;k++){
+    for(int r = 0;r < row1;r++){
+        for(int c = 0;c < col2;c++){
+            res[r][c] = 0;
+            res2[r][c] = 0;
+        }
+    }
+    for(int r = 0;r < row1;r++){
+        for(int c =0;c < col2;c++){
+            for(int k = 0;k < col1;k++){
                 res[r][c] += mat1[r][k] * mat2[k][c];
                 iteration1++;
             }
         }
     }
+    int outr = row1;
+    int ourc = col2;
     for(int r = 0;r < row1;r++){
-        short_vec vOutC = (short_vec)(0);
-        for(int c = 0;c < col2;c+=1){
-            short_vec vE1 = (short_vec)(0);
-            // cout<<r <<" "<<c<<endl;
-            short_vec pp = (short_vec)(mat1[r][c]);
-            // pp.print();
-            vE1 = *(short_vec *)mat2Idx;
-            // vE1.print();
-            short_vec resw = pp * vE1;
-            vOutC = vOutC + resw;
-            iteration2++;
-            cout<<mat2Idx<<endl;
-            mat2Idx+=32;
+        mat2Idx = &mat2[0][0];
+        for(int c = 0;c < col2;c+=32){
+            short_vec vOutC = (short_vec)(0);
+            for(int c = 0;c < col1;c+=1){
+                short_vec pp = (short_vec)(mat1[r][c]);
+                short_vec vE1 = *(short_vec *)mat2Idx;
+                short_vec resw = pp * vE1;
+                vOutC = vOutC + resw;
+                iteration2++;
+                mat2Idx+=32;
+            }
+            *(short_vec *) (res2Idx) = vOutC;
+            res2Idx += 32;
         }
-        // vOutC.print();
     }
-    // mat2Idx+=32;
-
-    // short_vec vE1 = *(short_vec *)(mat2Idx);
-    // vE1.print();
-    // cout<<mat2Idx<<endl;
-    // mat2Idx+=32;
-
-    // vE1 = (short_vec)(0);
-    // vE1 = *(short_vec *)mat2Idx;
-    // cout<<mat2Idx<<endl;
-    // cout<<&mat2[1][0]<<endl;
-    // vE1.print();
-    // vE1.print();
+    for(int r = 0;r < row1;r++){
+        for(int c = 0;c < col2;c++){
+            if(res[r][c] != res2[r][c] ){
+                cout<<"False :(";
+                return 0;
+            }
+        }
+    }
     cout<<"Iteration 1 : "<<iteration1<<endl;
     cout<<"Iteration 2 : "<<iteration2<<endl;
 }
